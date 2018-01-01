6045df1f7922e80817ef94f0aff187c048b174f7
Eric <eric@frianbiz.com>
2018-01-01 22:18:19 +0100

    refactor(env): replace  cross-env by a simple consts file copied on build

---
+++ environments/dev.env.ts
@@ -0,0 +1,7 @@
+
+export class Environment {
+    public static bddHost = 'xxx.xxx.xxxx.xxx';
+    public static bddName = 'OpenFoodFact';
+    public static bddLogin = '';
+    public static bddPassword = '';
+}
